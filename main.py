831d49a04b2b9f807bf1e4594a825bc357e4a773
AleksandarKacar <kacar95@gmail.com>
2023-08-07 15:17:58 +0200

    pokusaj pajanja databaze i getovanja kamijona (vraca internal server error)

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from models.Truck import Truck
+from get_db_pymongo import get_database
 
 app = FastAPI()
-    
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/trucks/")
+async def get_trucks():
+    try:
+        db = get_database()
+        collection = db.get_collection("trucks")
+        return list(collection.find())
+    except Exception as e:
+        print("An error occurred:", str(e))
+        
